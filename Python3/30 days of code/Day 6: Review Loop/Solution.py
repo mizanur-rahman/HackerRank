dc47fc74cf28f4c7e1e19dd5733582ef7ccb2655
Mizanur Rahman <52867202+mizanur-rahman@users.noreply.github.com>
2019-09-30 15:47:47 -0400

    Update Solution.py

--- Python3/30 days of code/Day 6: Review Loop/Solution.py
+++ Python3/30 days of code/Day 6: Review Loop/Solution.py
@@ -1,3 +1,8 @@
+#Task
+'''Given a string ,S, of length N  that is indexed from 0 to N-1 , print its even-indexed and odd-indexed characters as 2 space-separated strings on a single line (see the Sample below for more detail).
+
+Note: 0 is considered to be an even index.'''
+#Solution: 
 import math
 import os
 import random
