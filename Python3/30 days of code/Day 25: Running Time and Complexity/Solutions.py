f746f34781bb2eb1123c0bc3f320278d33280c4c
Mizanur Rahman <52867202+mizanur-rahman@users.noreply.github.com>
2020-03-10 16:41:10 -0400

    Update Solutions.py

--- Python3/30 days of code/Day 25: Running Time and Complexity/Solutions.py
+++ Python3/30 days of code/Day 25: Running Time and Complexity/Solutions.py
@@ -1,12 +1,16 @@
-T=int(input())
-for i in range(0,T):
-    n=int(input())
-    p=0
-    for i in range(2,int(n/2)+1):
+import math
+def checkPrime(n):
+    if n==1:
+        return False
+    for i in range(2,int(math.sqrt(n))+1):
         if n%i==0:
-            p=1
-            break
-    if n==1 or p==1:
-        print('Not prime')
-    else:
+            return False
+    return True
+
+T = int(input())
+for i in range(T):
+    n = int(input())
+    if checkPrime(n):
         print('Prime')
+    else:
+        print('Not prime')
